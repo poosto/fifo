7a8f7338f346f4a574402e31014711c2df92b959
Preston <prestonmeek6@gmail.com>
2026-03-31 00:23:28 +0200

    add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/prestonmeek/Desktop/PROJECTS/cppwork/stack
-# Build directory: /Users/prestonmeek/Desktop/PROJECTS/cppwork/stack/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[fifo_tests]=] "/Users/prestonmeek/Desktop/PROJECTS/cppwork/stack/build/test_runner")
-set_tests_properties([=[fifo_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/prestonmeek/Desktop/PROJECTS/cppwork/stack/CMakeLists.txt;21;add_test;/Users/prestonmeek/Desktop/PROJECTS/cppwork/stack/CMakeLists.txt;0;")
